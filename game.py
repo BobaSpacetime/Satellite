dffe7b54ddcbbb1243cedc82f237df49e8f82a5b
BobaSpacetime <163637926+BobaSpacetime@users.noreply.github.com>
2026-04-13 20:01:23 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -1,21 +1,46 @@
 import pgzrun
 import random
+import time
 
 WIDTH= 450
 HEIGHT= 450
-
+next = 0
+lines = []
 sat = []
+start = time.time()
 for i in range(10):
     s = Actor("satellite")
     s.x = random.randint(50, 409)
     s.y = random.randint(50, 409)
     sat.append(s)
-no = 1
 
 def draw():
+    global total
     screen.blit("background", (0,0))
+    no = 1
     for s in sat:
         s.draw()
-        screen.draw.text(str(no), (s.x, s.y))
+        screen.draw.text(str(no), (s.x - 10, s.y - 30), color="white")
+        no += 1
+    for l in lines:
+        screen.draw.line(l[0],l[1], "#FF0000")
+    if next < 10:
+        total = time.time() - start
+        screen.draw.text(str(round(total, 2)), (10,30), color="white")
+    else:
+        screen.draw.text(str(round(total, 2)), (10,30), color="white")
+
+def on_mouse_down(pos):
+    global next, lines
+    if sat[next].collidepoint(pos):
+        if next > 0:
+            lines.append((sat[next-1].pos, sat[next].pos))
+        next+=1
+    else:
+        lines = []
+        next = 0
+
+def update():
+    pass
 
 pgzrun.go()
